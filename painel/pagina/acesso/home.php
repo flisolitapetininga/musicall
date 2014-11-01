30d3278a207c14678065c9daf41267d7668f6a1f
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-01 18:12:04 +0000

    query acesso e crud

--- painel/pagina/acesso/home.php
+++ painel/pagina/acesso/home.php
@@ -1,6 +1,6 @@
 <div><image src="../imagem/logo.png" width="400" height="135"/></div>
 <div>
-    <form method="post" action="">
+    <form method="post" action="./pagina/acesso/query.php">
         <input type="text" name="username" placeholder="Nome do usuário"/>
         <input type="password" name="senha" placeholder="Senha do usuário"/>
         <input type="button" name="acessar" value="acessar"/>
