e73b09d68e210dfb02be367ad7a5a5ed80dd3f3c
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-01 19:08:57 +0000

    submit e consulta de acesso

--- painel/pagina/acesso/home.php
+++ painel/pagina/acesso/home.php
@@ -1,8 +1,20 @@
 <div><image src="../imagem/logo.png" width="400" height="135"/></div>
 <div>
-    <form method="post" action="./pagina/acesso/query.php">
+    <form method="post" action="./pagina/acesso/query.php" id="formacesso">
         <input type="text" name="username" placeholder="Nome do usuário"/>
         <input type="password" name="senha" placeholder="Senha do usuário"/>
         <input type="button" name="acessar" value="acessar"/>
     </form>
 </div>
+
+<script type="text/javascript">
+	window.onload = function(){
+		var $obj = document.getElementsByTagName('input');
+			$obj[2].onclick = function(){
+				if($obj[0].value != '' && $obj[1].value != ''){
+					document.getElementById('formacesso').submit();
+				}
+			}
+	}
+	console.log('Desenvolvido por Gustavo Mathias');
+</script>
--- painel/pagina/acesso/query.php
+++ painel/pagina/acesso/query.php
@@ -1,3 +1,10 @@
 <?php
+	require_once '../../../core/conexao.php';
+	require_once '../../../core/crud.php';
+	
+	
+	
+	echo $_POST['username'];
+	echo $_POST['senha'];
 
 ?>
