3d89d1ecb11ab16e803a3c85833fe91ed12f0ce5
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-01 18:00:04 +0000

    select do crud

---
+++ core/crud.php
@@ -0,0 +1,29 @@
+<?php
+	class crud{
+		private $tabela;
+		private $campo;
+		private $condicao;
+		private $valor;
+		
+		public function setTabela($tabela){
+			$this->tabela = $tabela;
+		}
+		public function setCampo($campo){
+			$this->campo = $campo;
+		}
+		public function setCondicao($condicao){
+			$this->condicao = $condicao;
+		}
+		public function setValor($valor){
+			$this->valor = $valor;
+		}
+		
+		public function select(){
+			$sql = 'SELECT '.$this->campo.' FROM '.$this->tabela.((isset($this->condicao))? $this->condicao : '');
+			$query = mysql_query($sql);
+			$query = mysql_fetch_object($query);
+			
+			return $query;
+		}
+	}
+?>
