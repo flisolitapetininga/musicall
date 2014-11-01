b5f4e5459c8b8254bba09d8c452ffb9d12de24f4
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-01 20:00:07 +0000

    CSS

--- painel/controle.php
+++ painel/controle.php
@@ -3,7 +3,14 @@ session_start();
 
 if(isset($_SESSION['radio'])){
     $pagina = array('inicio');
-    require_once (isset($_GET['p']) && in_array($_GET['p'],$pagina))?'pagina/'.$_GET['p'].'/home.php':'pagina/inicio/home.php';
+    
+    //estrutura de pagina
+    echo '<div id="e">';
+        require_once 'estrutura/menu.php';
+    echo '</div>';
+    echo '<div id="d">';
+        require_once (isset($_GET['p']) && in_array($_GET['p'],$pagina))?'pagina/'.$_GET['p'].'/home.php':'pagina/inicio/home.php';
+    echo '</div>';
 }else{
     require_once 'pagina/acesso/home.php';
 }
--- painel/index.php
+++ painel/index.php
@@ -2,7 +2,8 @@
 <html lang="pt-BR">
     <head>
         <meta charset="utf-8"/>
-        <title>Rádio MusicAll</title>
+        <title>Rádio MusicAll - Painel</title>
+        <link rel=”stylesheet” type=”text/css” href=”../core/painel.css” />
     </head>
     <body>
     	<?php require_once 'controle.php'; ?>
