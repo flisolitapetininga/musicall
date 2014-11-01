b24c4fe605fa0956dffad327a42c6519a4f26f7e
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-01 17:29:14 +0000

    git ignore conexao

--- core/conexao.php
+++ core/conexao.php
@@ -1,8 +1,16 @@
 <?php
 	class conectar{
-		private $local;
-		private $banco;
-		private $usuario;
-		private $senha;
+		private $local = '';
+		private $banco = '';
+		private $usuario = '';
+		private $senha = '';
+		
+		public function con(){
+			$c = mysql_connect($this->local, $this->usuario, $this->senha);
+			$c = mysql_select_db($this->banco, $c);
+			
+			return $c;
+		}
+		
 	}
 ?>
