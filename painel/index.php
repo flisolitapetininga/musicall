318c47e82e641344555647661c7cf42625e19e5b
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-03 16:27:21 +0000

    erro besta

--- painel/index.php
+++ painel/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8"/>
         <title>Rádio MusicAll - Painel</title>
-        <link rel=”stylesheet” type=”text/css” href=”../core/painel.css” />
+        <link rel="stylesheet" href="../core/painel.css" />
     </head>
     <body>
     	<?php require_once 'controle.php'; ?>
