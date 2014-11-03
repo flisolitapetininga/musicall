798361ac15bf26c47a18483d4459165199b900ed
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-03 19:54:16 +0000

    logs

--- painel/estrutura/menu.php
+++ painel/estrutura/menu.php
@@ -3,5 +3,6 @@
     <a href="?p=inicio"><li>início</li></a>
     <a href="?p=cliente"><li>cliente</li></a>
     <a href="?p=radio"><li>rádio</li></a>
+    <a href="?p=radio"><li>logs</li></a>
     <a href="?p=sair"><li>sair</li></a>
 </ul>
