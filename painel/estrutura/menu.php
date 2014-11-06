d1333ca097d001f1dd910804b13242b05cce34ea
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-06 19:38:37 +0000

    add option no menu

--- painel/estrutura/menu.php
+++ painel/estrutura/menu.php
@@ -3,6 +3,7 @@
     <a href="?p=inicio"><li>início</li></a>
     <a href="?p=cliente"><li>cliente</li></a>
     <a href="?p=radio"><li>rádio</li></a>
-    <a href="?p=radio"><li>logs</li></a>
+    <a href="?p=relatorio"><li>relatórios</li></a>
+    <a href="?p=log"><li>logs</li></a>
     <a href="?p=sair"><li>sair</li></a>
 </ul>
