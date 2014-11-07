6c4ad8e34b5ffed609637b2d0c3df28035249e68
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-07 16:07:49 +0000

    conexao

--- painel/core/conexao.php
+++ painel/core/conexao.php
@@ -1,9 +1,9 @@
 <?php
 	class conectar{
-		private $local = 'localhost';
-		private $banco = 'musicall';
-		private $usuario = 'root';
-		private $senha = '81224773';
+		private $local = '';
+		private $banco = '';
+		private $usuario = '';
+		private $senha = '';
 		
 		public function con(){
 			$c = mysql_connect($this->local, $this->usuario, $this->senha);
