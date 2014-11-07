46f1c5f48b6f8aaeb17f5c537020168e96dc8f38
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-07 15:39:24 +0000

    teste

---
+++ core/crud.php
@@ -0,0 +1,33 @@
+<?php
+	class crud{
+		private $tabela;
+		private $campo;
+		private $condicao;
+		private $valor;
+		
+		public function setTabela($tabela){
+			$this->tabela = $tabela;
+		}
+		public function setCampo($campo){
+			$this->campo = $campo;
+		}
+		public function setCondicao($condicao){
+			$this->condicao = $condicao;
+		}
+		public function setValor($valor){
+			$this->valor = $valor;
+		}
+		
+		public function select(){
+			$sql = 'SELECT '.$this->campo.' FROM '.$this->tabela.((isset($this->condicao))? ' WHERE '.$this->condicao : '');
+			$query = mysql_query($sql) or die ('erro na consulta:: '.mysql_error());
+			$query = mysql_fetch_object($query);
+			return $query;
+		}
+		public function insert(){
+		    $sql = 'INSERT INTO '.$this->tabela.' ('.$this->campo.') VALUES ('.$this->valor.')';
+		    $query = mysql_query($sql) or die ('erro na consulta:: '.mysql_error());
+		    return $query;
+		}
+	}
+?>
