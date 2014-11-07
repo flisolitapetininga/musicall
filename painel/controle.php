f6a8a38854ef8e0b73f200650c6a00c8ccdd6e98
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-07 16:23:48 +0000

    pagina

--- painel/controle.php
+++ painel/controle.php
@@ -3,14 +3,14 @@ session_start();
 
 if(isset($_SESSION['radio'])){
     $usuario = json_decode($_SESSION['radio']);
-    $pagina = array('inicio','sair');
+    $pagina = array('inicio','arquivo','sair');
     
     //estrutura de pagina
     echo '<div id="e">';
         require_once 'estrutura/menu.php';
     echo '</div>';
     echo '<div id="d">';
-        require_once (isset($_GET['p']) && in_array($_GET['p'],$pagina))?'pagina/'.$_GET['p'].'/home.php':'pagina/inicio/home.php';
+        require_once 'pagina/inicio/home.php';
     echo '</div>';
 }else{
     require_once 'pagina/acesso/home.php';
--- painel/estrutura/menu.php
+++ painel/estrutura/menu.php
@@ -1,4 +1,6 @@
 <ul>
     <div>Seja bem vindo <?=$usuario->nome;?></div>
+    <a href="?p=inicio"><li>.htpasswd</li></a>
+    <a href="?p=arquivo"><li>htaccess</li></a>
     <a href="?p=sair"><li>sair</li></a>
 </ul>
