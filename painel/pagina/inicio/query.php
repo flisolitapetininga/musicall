3b1ba9358aaeba18beee5c367bc2f85e1d301b94
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-10 17:26:18 +0000

    query arquivo

--- painel/pagina/inicio/query.php
+++ painel/pagina/inicio/query.php
@@ -4,10 +4,14 @@
     if($_GET['a'] == 'criar'){
         exec("htpasswd -c .htpasswd ".$_POST['nome']);
         exec($_POST['senha']);    
+        // exec($_POST['senha']);
     }elseif($_GET['a'] == 'salvar'){
-        exec("htpasswd -sd .htpasswd ".$_POST['nome']);
+        exec("htpasswd .htpasswd ".$_POST['nome']);
         exec($_POST['senha']);
+        // exec($_POST['senha']);
+        
     }
+    // exec('sudo chown kerubin2:kerubin2');
     
-    header('Location: ../../?p=inicio')
+    header('Location: ../../?p=inicio');
 ?>
