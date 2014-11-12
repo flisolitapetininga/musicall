0a597357d2cacaa438a2fb452fddd2f764694537
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-12 15:43:50 +0000

    htaccess diretorio automatico

--- painel/pagina/arquivo/query.php
+++ painel/pagina/arquivo/query.php
@@ -1,12 +1,10 @@
 <?php
-    $local = '/home/kerubin2/Web/musicall/';
-    
     chdir ('../../../');
 
-    $string = "AuthType Basic\nAuthName \"Restricted access\"\nAuthUserFile ".$local.".htpasswd\nrequire valid-user";
+    $string = "AuthType Basic\nAuthName \"Restricted access\"\nAuthUserFile ".getcwd()."/.htpasswd\nrequire valid-user";
     $arquivo = fopen('.htaccess','w');
     fwrite($arquivo, $string);
     fclose($arquivo);
     
-    header('Location: ../../?p=arquivo')
+    header('Location: ../../?p=arquivo');
 ?>
