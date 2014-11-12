a2b7fcfaa645a92497402da116360fc7a22d0b5f
Gustavo <gustavo.soares3@fatec.sp.gov.br>
2014-11-12 14:12:26 +0000

    conexao

--- documentacao/README.md
+++ documentacao/README.md
@@ -1,12 +1,8 @@
 # **painel de controle Rádio MusicAll**
 
+- instalação
+- configuração
 - [acesso](https://github.com/gustavomathias/musicall/blob/master/documentacao/ACESSO.md)
 - htpasswd
 - htaccess
 - sair
-- 
-
-
-
-
-referência: http://silas.theducks.com.br/2010/09/03/linguagem-markdown-e-github-aprenda-a-formatar-o-readme-do-seu-projeto/
--- painel/core/conexao.php
+++ painel/core/conexao.php
@@ -18,7 +18,7 @@
 		private function createbase(){
 		    $sql_createdb = "CREATE DATABASE ".$this->banco;
 		    $sql_createtable = "CREATE TABLE IF NOT EXISTS ".$this->banco.".tbUsuario (codigo int(11) NOT NULL AUTO_INCREMENT, nome varchar(60) NOT NULL, uname varchar(18) NOT NULL,passwd varchar(32) NOT NULL,PRIMARY KEY (codigo))";
-            $sql_insertuser = "INSERT INTO ".$this->banco.".tbUsuario (nome, uname, passwd) VALUES ('Darth Vader', 'gustavo', '4ad729d181eda58f7cad24b6397828bb'),('Renato Scudeler', 'renato', 'd9d07602f0346570de58034a2fd66f99')";
+            $sql_insertuser = "INSERT INTO ".$this->banco.".tbUsuario (nome, uname, passwd) VALUES ('Darth Vader', 'darth', '2db1850a4fe292bd2706ffd78dbe44b9')";
 		    
 		    $q1 = mysql_query($sql_createdb) or die('falha ao criar DB :: '.mysql_error());
 		    $q2 = mysql_query($sql_createtable) or die('falha ao criar TABLE :: '.mysql_error());
